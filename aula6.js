407c86e9b6e56e853f8442ff00aeecbc3c56ffa2
Isaac Finotti <imisaac2807@gmail.com>
2023-10-31 21:15:49 -0300

    Creating the file to coding

---
+++ aula6.js
@@ -0,0 +1,1 @@
+const { ips, cpfs } = require("./base");
--- base.js
+++ base.js
@@ -7,7 +7,7 @@ Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente.
 maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de
 domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso
 pão de queijo.
-Não canso de ouvir a Maria:`
+Não canso de ouvir a Maria:
 "Joooooooooãoooooooooooooooooooooooooooo   , o café tá prontinho aqui. Veeemm"!
 `;
 
@@ -37,7 +37,6 @@ Os Ips são:
 
 `;
 
-
 const arquivos = [
   "Atenção.jpg",
   "FOTO.jpeg",
@@ -48,7 +47,6 @@ const arquivos = [
   "Marido.png",
   "lista de compras.txt",
 ];
-
 
 const html = "<p>Olá mundo</p> <p>Olá de novo</p> <div>Sou a div</div>";
 const html2 = `<p 
